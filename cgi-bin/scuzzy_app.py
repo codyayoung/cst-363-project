1ec2dca99dd5cd6653507f51c299d0078dbb5263
Nathan Warren-Acord <nacord26@gmail.com>
2019-01-28 19:23:10 -0800

    Updated html/javascript to send field values to python script.

--- cgi-bin/scuzzy_app.py
+++ cgi-bin/scuzzy_app.py
@@ -3,26 +3,20 @@ import mysql.connector
 cgitb.enable()
 form = cgi.FieldStorage()
 
-def print_data(table):
-	print("<form action=\"/cgi-bin/scuzzy_output.py\" method = \"post\">")
-	if table == "artists":
-		print("<h2>Search by artist name or genre.</h2>")
-		print("<h2>Leave all fields blank to print all artists.</h2>")
-		print("Artist name:\t <input type = \"text\" name = \"artist\"/></br>")
-		print("Genre:\t <input type = \"text\" name = \"genre\"/></br>")
-		print("<input type=\"submit\" value=\"search\" name=\"Search\"/>")
-	print("</form>")
+temp = []
 
-selection = form["queries"].value
+for item in form.value:
+	temp.append(item)
 
-operation = selection.split(' ',1)[0] ##Get first word (desired operation)
-table = selection.split(' ',1)[1] ##Get second word (table name)
+##selection = form["queries"].value
+
+##operation = selection.split(' ',1)[0] ##Get first word (desired operation)
+##table = selection.split(' ',1)[1] ##Get second word (table name)
 
 print("Content-Type: text/html")
 print()
 print("<html><body>")
 
-if "print" in operation:
-	print_data(table)
+print("{}".format(temp))
 
 print("</html></body>")
--- cgi-bin/scuzzy_output.py
+++ cgi-bin/scuzzy_output.py
@@ -1,1 +1,14 @@
 print("Hello")
+
+def print_data(table):
+	print("<form action=\"/cgi-bin/scuzzy_output.py\" method = \"post\">")
+	if table == "artists":
+		print("<h2>Search by artist name or genre.</h2>")
+		print("<h2>Leave all fields blank to print all artists.</h2>")
+		print("Artist name:\t <input type = \"text\" name = \"artist\"/></br>")
+		print("Genre:\t <input type = \"text\" name = \"genre\"/></br>")
+		print("<input type=\"submit\" value=\"search\" name=\"Search\"/>")
+	print("</form>")
+
+	if "print" in operation:
+	print_data(table)
