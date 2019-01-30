f13b492159e4fe54869479fcdf32f0cacf0c0b6a
Nathan Warren-Acord <nacord26@gmail.com>
2019-01-29 21:33:11 -0800

    Ready for final testing. HTML file just needs comments.

--- cgi-bin/scuzzy_app.py
+++ cgi-bin/scuzzy_app.py
@@ -1,28 +1,35 @@
+##########################
+## SCSI Logic
+## Nathan Warren-Acord
+## Cody Young
+## Abby Packham
+##
+## CST 363 Databases - Project 1
+##########################
+
 from scuzzy_sql_queries import *
 import cgitb, cgi
 import mysql.connector
 cgitb.enable()
 form = cgi.FieldStorage()
 
-#user_query = []
-user_query = {}
+user_query = {} #Holds the user input
 selection = ""
 
+#Find what operation and table has been selected
 for item in form:
 	if form[item].value == "Submit":
-		#user_query.append(item)
 		selection = item
 		
-
+#Save any values that the user has entered
 for item in form.value:
 	if item.value == "Submit":
 		continue
 	else:
-		#user_query.append((item.name, item.value))
 		user_query[item.name] = item.value
 
-operation = selection.split(' ',1)[0] ##Get first word (desired operation)
-table = selection.split(' ',1)[1] ##Get second word (table name)
+operation = selection.split(' ',1)[0] #Get first word (desired operation)
+table = selection.split(' ',1)[1] #Get second word (table name)
 
 print("Content-Type: text/html")
 print()
@@ -41,14 +48,19 @@ cnx = mysql.connector.connect(user='root',
                                 database='scuzzy_records',
                                 host='127.0.0.1')
 
+#Cursor for our select statements
 cursor = cnx.cursor()
+#Cursor for other queries
 alter_cursor = cnx.cursor()
+#Strings to hold the query text
 sql = ""
 alter_sql = ""
+#List that will be filled with relevant table headers
 table_headers = []
+#Prompt the user if an add, remove, or update was successful
 status_prompt = ""
 
-##Handles queries for artists
+#Handles queries for artists
 if table == "artists":
 	table_headers = ["Artist", "Genre"]
 	if operation == "print":
@@ -57,15 +69,12 @@ if table == "artists":
 		sql = artist_print({})
 	if operation == "add":
 		alter_sql = artist_add(user_query)
-		status_prompt = "<h1>Artist added successfully.</h1><br><br>"
 	elif operation == "remove":
 		alter_sql = artist_remove(user_query)
-		status_prompt = "<h1>Artist removed successfully.</h1><br><br>"
 	elif operation == "update":
 		alter_sql = artist_update(user_query)
-		status_prompt = "<h1>Artist updated successfully.</h1><br><br>"
 
-##Handles queries for venues
+#Handles queries for venues
 elif table == "venues":
 	table_headers = ["Venue ID", "City", "State", "Capacity", "Cost"]
 	if operation == "print":
@@ -74,9 +83,8 @@ elif table == "venues":
 		sql = venue_print({})
 	if operation == "update":
 		alter_sql = venue_update(user_query)
-		status_prompt = "<h1>Venue updated successfully.</h1><br><br>"
 
-##Handles queries for record sales
+#Handles queries for record sales
 elif table == "record_sales":
 	table_headers = ["Sale ID", "Artist", "Date of Sale", "Total Amount", "City", "State"]
 	if operation == "print":
@@ -85,24 +93,36 @@ elif table == "record_sales":
 		sql = sale_print({})
 	if operation == "add":
 		alter_sql = sale_add(user_query)
-		status_prompt = "<h1>Sale added successfully.</h1><br><br>"
 
-##Handles queries for performances
+#Handles queries for performances
 elif table == "performances":
-	pass
+	table_headers = ["Performance name", "Performing artist", "Total cost", "Date of Performance", "City", "State"]
+	if operation == "print":
+		sql = performance_print(user_query)
+	else:
+		sql = performance_print_print({})
+	if operation == "add":
+		alter_sql = performance_add(user_query)
+	elif operation == "remove":
+		alter_sql = performance_remove(user_query)
+	elif operation == "update":
+		alter_sql = performance_update(user_query)
 
-##If something goes terribly wrong...
+#If something goes terribly wrong...
 else:
-	print("<h1>Unknown error. Please go back and try again.</h1>")
+	print("<h1>Unknown error. Don't panic. Please go back and try again.</h1>")
 	cnx.close()
 	raise(SystemExit)
 
-if len(status_prompt) > 0:
-	print(status_prompt)
-
-if len(alter_sql) > 0:
+#Check if add, remove, or update queries were successful
+if alter_sql:
+	status_prompt = "<h1>{} {}ed successfully.</h1><br>".format(table, operation)
 	alter_cursor.execute(alter_sql)
+if alter_sql == False:
+	status_prompt = "<h1>{} failed to {}. Please fill in all fields.</h1><br>".format(table, operation)
+print(status_prompt)
 
+#Print the table based on the query
 cursor.execute(sql)
 print('<table border="1"><tr>')
 for label in table_headers:
@@ -114,11 +134,10 @@ for item in row:
 	for value in item:
 		print('<td>{} '.format(value))
 	print('</tr>')
-#print("<br>{}<br>".format(sql))
-#print ("{}<br>{}<br>".format(operation, table))
-#print("{}".format(user_query))
+
+#Button to redirect back to main page
 print("<p><a href=\"/scuzzy_app.html\" class=\"button\">Main Menu</a></p>")
 print("</body></html>")
 
 cnx.commit()
-cnx.close()  ## close the connection 
+cnx.close()  #Close the connection 
--- cgi-bin/scuzzy_sql_queries.py
+++ cgi-bin/scuzzy_sql_queries.py
@@ -1,11 +1,17 @@
 #############
-#SQL queries#
+# SCSI Logic
+# Nathan Warren-Acord
+# Cody Young
+# Abby Packham
+#
+# CST 363 - Databases - Project 1 SQL queries
 #############
 
 #########
 #Artists#
 #########
 
+#Prints the artist table.
 def artist_print(values):
 	print_artists = """SELECT a.artist_name, g.genre_name
 				FROM artists a JOIN genres g 
@@ -19,8 +25,10 @@ def artist_print(values):
 		print_artists += " WHERE g.genre_name = '{}'".format(values["genre"])
 		return print_artists
 
+#Adds artist
+#Adds an artist to the artist table.
 def artist_add(values):
-	if len(values < 2):
+	if len(values) < 2:
 		return False
 	add_artist = "INSERT INTO artists VALUES (NULL, '{}', (SELECT genre_id FROM genres WHERE genre_name = '{}'));".format(values["name"], values["genre"])
 	return add_artist
@@ -50,6 +58,7 @@ def artist_update(values):
 #Venues#
 ########
 
+#Prints the venue table.
 def venue_print(values):
 	print_venue = """SELECT venue_id, 
 					venue_city, 
@@ -62,9 +71,11 @@ def venue_print(values):
 	print_venue += "WHERE "
 	for item in values:
 		print_venue += "venue_{} = '{}' AND ".format(item, values[item])
-	print_venue.strip(' AND ')
+	print_venue = print_venue.strip(' AND ')
 	return print_venue
 
+#Update venue
+#Updates a venue on the venue table.
 def venue_update(values):
 	if "update" not in values:
 		return False
@@ -79,6 +90,7 @@ def venue_update(values):
 #Sales#
 #######
 
+#Print the record sales table.
 def sale_print(values):
 	print_sale = """SELECT sales_id,
 					a.artist_name,
@@ -89,17 +101,19 @@ def sale_print(values):
 					FROM record_sales r JOIN artists a ON r.artist_id = a.artist_id"""
 	if len(values) < 1:
 		return print_sale
-	print_sale += "WHERE "
+	print_sale += " WHERE "
 	for item in values:
 		if item == "artist":
-			print_sale += "(SELECT artist_id FROM artists WHERE artist_name = '{}') = artist_id AND ".format(values["artist"])
+			print_sale += "a.artist_name = '{}' AND ".format(values["artist"])
 		else:
 			print_sale += "sales_{} = '{}' AND ".format(item, values[item])
-	print_sale.strip(' AND ')
+	print_sale = print_sale.strip(' AND ')
 	return print_sale
 
+#Add sale
+#Adds a sale to the record sale table.
 def sale_add(values):
-	if len(values < 5):
+	if len(values) < 5:
 		return False
 	add_sale = "INSERT INTO record_sales VALUES (NULL, (SELECT artist_id FROM artists WHERE artist_name = '{}'), '{}', {}, '{}', '{}');".format(values["artist"], values["date"], values["total"], values["city"], values["state"])
 	return add_artist
@@ -108,14 +122,59 @@ def sale_add(values):
 #Performances#
 ##############
 
+#Prints the performance table
 def performance_print(values):
-	return False
+	print_performance = """SELECT p.performance_name,
+							a.artist_name,
+							p.performance_cost,
+							p.performance_date,
+							v.venue_city,
+							v.venue_state
+							FROM performances p JOIN artists a ON a.artist_id = p.performing_artist_id JOIN venues v ON p.venue_id = v.venue_id"""
+	if len(values) < 1:
+		return print_performance
+	print_performance += " WHERE "
+	for item in values:
+		if item == "artist":
+			print_performance += "a.artist_name = '{}' AND ".format(values["artist"])
+		if item == "cost" or item == "date":
+			print_performance += "p.performance_{} = {} AND ".format(item, values[item])
+		if item == "city" or item == "state":
+			print_performance += "v.venue_{} = {} AND ".format(item, values[item])
+	print_performance = print_performance.strip(' AND ')
+	return print_performance
 
+#Add performance
+#Adds a performance to the performance table.
 def performance_add(values):
-	pass
+	if len(values) < 5:
+		return False
+	add_performance = "INSERT INTO performances VALUES (NULL, '{}', (SELECT artist_id FROM artists WHERE artist_name = '{}'), {}, {}, {};".format(values["name"], values["artist"], values["cost"], values["date"], values["venue"])
+	return add_performance
 
+#Remove performance
+#Removes a performance from the performance table.
 def performance_remove(values):
-	pass
+	if len(values) < 2:
+		return False
+	remove_performance = "DELETE FROM performance WHERE performance_name = '{}' AND performance_date = {};".format(values["name"], values["date"])
+	return remove_performance
 
+#Update performance
+#Updates a performance from the performance table.
 def performance_update(values):
-	pass
+	if "update" not in values:
+		return False
+	update_performance = "UPDATE performances SET "
+	for item in values:
+		if item == "artist":
+			update_performance += "performing_artist_id = (SELECT artist_id FROM artists WHERE artist_name = '{}')".format(values["artist"])
+		elif item == "venue":
+			update_performance += "venue_id = {}".format(values["venue"])
+		elif item == "name":
+			update_performance += "performance_{} = '{}', ".format(item, values[item])
+		else:
+			update_performance += "performance_{} = {}, ".format(item, values[item])
+	update_performance.strip(', ')
+	update_performance += " WHERE performance_name = {};".format(values["update"])
+	return update_performance
